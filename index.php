096737de0017223ce3b3608a40f576fea56f98a8
benaor <benjamingirard25@gmail.com>
2020-08-17 23:27:30 +0200

    structure HTML

--- index.php
+++ index.php
@@ -1,3 +1,65 @@
-<?php
-//Starting project ...
-echo 'hello world';
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="design/style.css">
+    <link rel="icon" href="design/pictures/favico.png">
+</head>
+
+<body>
+    <!-- section hello -->
+    <section id="hello">
+
+        <!-- container -->
+        <div class="container">
+
+            <!-- header -->
+            <header>
+                <img src="design/pictures/logo.png" alt="logo" id="logo">
+            </header>
+
+            <!-- Value proposition -->
+            <h1>URL trop longue ? raccourcissez-là.</h1>
+            <h2>Largement meilleur et plus court que les autres !</h2>
+
+            <!-- Form -->
+            <form action="index.php" method="post">
+                <input type="url" name="url" placeholder="entrez l'url a raccourcir">
+                <input type="submit" value="raccourcir">
+            </form>
+
+        </div>
+    </section>
+
+    <!-- Section Brands -->
+    <section id="brands">
+
+        <!-- container -->
+        <div class="container">
+
+            <h3>Ces marques nous font confiance</h3>
+            <img src="design/pictures/1.png" alt="1" class="picture">
+            <img src="design/pictures/2.png" alt="2" class="picture">
+            <img src="design/pictures/3.png" alt="3" class="picture">
+            <img src="design/pictures/4.png" alt="4" class="picture">
+
+        </div>
+
+    </section>
+
+    <!-- FOOTER -->
+    <footer>
+        <img src="design/pictures/logo2.png" alt="logo" id="logo"><br>
+        2018 © Bitly<br>
+        <a href="#">Contact</a> - <a href="#">À propos</a>
+    </footer>
+
+
+</body>
+
+</html>
